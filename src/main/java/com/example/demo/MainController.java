6229bb4e3e183d9845f36f4142e74e5396710617
Joshua Tucker <jptucker2009@gmail.com>
2018-08-06 14:25:49 -0400

    Create MainController.java

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,38 @@
+@Controller
+public class HomeController {
+
+    @Autowired
+    ActorRepository actorRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        // First let's create an actor
+        Actor actor = new Actor();
+        actor.setName("Sandra Bullock");
+        actor.setRealname("Sandra Mae Bullowski");
+
+        // Now let's create a movie
+        Movie movie = new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+        movie.setDescription("About Emojis...");
+
+        // Add the movie to an empty list
+        Set<Movie> movies = new HashSet<Movie>();
+        movies.add(movie);
+
+        // Add the list of movies to the actor's movie list
+        actor.setMovies(movies);
+
+        // Save the actor to the database
+        actorRepository.save(actor);
+
+        // Grad all the actors from the database and send them to
+        // the template
+        model.addAttribute("actors", actorRepository.findAll());
+        return "index";
+    }
+}
